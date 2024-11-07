d684549647f9d7907e6e9335d0782dc0d8eed21e
danviciu <danvoicuteodorescu@gmail.com>
2024-11-07 18:43:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function startSimulation() {
+  alert("Începem simularea! (Adaugă aici detalii sau link către simulări.)");
+}
+
+function startQuiz() {
+  alert("Începem quiz-ul! (Poți redirecționa utilizatorul către întrebările de quiz.)");
+}
